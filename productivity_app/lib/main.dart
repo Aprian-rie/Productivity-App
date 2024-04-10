4caccecde4a7a08bf9892906d66c2fd90abc633e
Aprian-rie <aprythomas5@gmail.com>
2024-04-10 20:26:44 +0300

    Appbar plus bottom bar

---
+++ productivity_app/lib/main.dart
@@ -0,0 +1,124 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'models/global.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Productivity App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Productivity App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      color: Colors.yellow,
+      home: SafeArea(
+        child: DefaultTabController(
+          length: 3,
+          child: Scaffold(
+            body: Stack(
+              children: <Widget>[
+                TabBarView(
+                  children: [
+                    Container(
+                      color: darkGreyColor,
+                    ),
+                    Container(
+                      color: Colors.orange,
+                    ),
+                    Container(
+                      color: Colors.lightGreen,
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 50),
+                  height: 160,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(50),
+                        bottomRight: Radius.circular(50)),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        "Intray",
+                        style: intrayTitle,
+                      ),
+                      Container()
+                    ],
+                  ),
+                ),
+                Container(
+                  height: 80,
+                  width: 80,
+                  margin: EdgeInsets.only(
+                      top: 120,
+                      left: MediaQuery.of(context).size.width * 0.5 - 40),
+                  child: FloatingActionButton(
+                      onPressed: () {},
+                      shape: CircleBorder(),
+                      backgroundColor: redColor,
+                      child: Icon(
+                        Icons.add,
+                        size: 80,
+                      )),
+                )
+              ],
+            ),
+            appBar: AppBar(
+              elevation: 0,
+              title: TabBar(
+                tabs: [
+                  Tab(
+                    icon: Icon(Icons.home),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.rss_feed),
+                  ),
+                  Tab(
+                    icon: Icon(Icons.perm_identity),
+                  ),
+                ],
+                labelColor: darkGreyColor,
+                unselectedLabelColor: Colors.blue,
+                indicatorSize: TabBarIndicatorSize.label,
+                indicatorPadding: EdgeInsets.all(5.0),
+                indicatorColor: Colors.transparent,
+              ),
+            ),
+            backgroundColor: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ productivity_app/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = Color(0xFF212128);
+Color redColor = Color(0xFFE33E57);
+
+TextStyle intrayTitle = TextStyle(
+    fontFamily: 'Avenir',
+    fontWeight: FontWeight.bold,
+    color: darkGreyColor,
+    fontSize: 40);
